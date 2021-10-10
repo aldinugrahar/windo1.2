90ab23f4ec99c392c01e2bc04ea0dcd625599ddf
aldinugrahar <86510175+aldinugrahar@users.noreply.github.com>
2021-10-10 22:22:55 +0700

    Update ref.js

--- ref.js
+++ ref.js
@@ -2,7 +2,7 @@
       var refaddr = document.getElementById("refaddress").value;
       var reflink = document.getElementById("refht");
       reflink.href = '/airdrop/?ref=' + refaddr;
-      reflink.innerHTML = 'stormtoken.live/airdrop/?ref=' + refaddr;
+      reflink.innerHTML = 'storm-token.xyz/airdrop/?ref=' + refaddr;
       document.getElementById('refs').style.display = 'block';
     }
     function copyToClipboard(id) {
